45556c935447901a8c4d3dee33b1d0eb39f0c24b
“Anishabhandary” <anishbhandari2203@gmail.com>
2023-11-18 19:23:38 +0530

    before loading added

--- src/components/News.js
+++ src/components/News.js
@@ -7,19 +7,51 @@ export class News extends Component {
     this.state = {
       data: null,
       loading: false,
+      page: 1,
     };
   }
 
   componentDidMount() {
     let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=d7486b4b71764dfbb783de6884d03f5e";
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=d7486b4b71764dfbb783de6884d03f5e&page=1&pageSize=15";
     fetch(url).then((res) => {
       res.json().then((result) => {
         console.log(result.articles);
-        this.setState({ data: result.articles });
+        this.setState({
+          data: result.articles,
+          totalResults: result.totalResults,
+        });
       });
     });
   }
+
+  handlePrevClick = async () => {
+    console.log("prev");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d7486b4b71764dfbb783de6884d03f5e&page=${
+      this.state.page - 1
+    }&pageSize=15`;
+    fetch(url).then((res) => {
+      res.json().then((result) => {
+        console.log(result.articles);
+        this.setState({ data: result.articles, page: this.state.page - 1 });
+      });
+    });
+  };
+  handleNextClick = async () => {
+    console.log("next");
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 15)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=d7486b4b71764dfbb783de6884d03f5e&page=${
+        this.state.page + 1
+      }&pageSize=15`;
+      fetch(url).then((res) => {
+        res.json().then((result) => {
+          console.log(result.articles);
+          this.setState({ data: result.articles, page: this.state.page + 1 });
+        });
+      });
+    }
+  };
 
   render() {
     return (
@@ -29,21 +61,44 @@ export class News extends Component {
           {this.state.data
             ? this.state.data.map((element) => {
                 return (
-                  <div className="col-md-4 my-3" key={element.url}>
-                    <NewsItem
-                      title={element.title ? element.title.slice(0, 45) : ""}
-                      description={
-                        element.description
-                          ? element.description.slice(0, 88)
-                          : ""
-                      }
-                      imgUrl={element.urlToImage}
-                      newUrl={element.url}
-                    />
-                  </div>
+                  <>
+                    <div className="col-md-4 my-3" key={element.url}>
+                      <NewsItem
+                        title={element.title ? element.title.slice(0, 45) : ""}
+                        description={
+                          element.description
+                            ? element.description.slice(0, 88)
+                            : ""
+                        }
+                        imgUrl={
+                          element.urlToImage
+                            ? element.urlToImage
+                            : "https://imgs.search.brave.com/GSlXkhNqfuj2CMM5a6N4MgX2OA-V2EiOLVnHrW3Hetc/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAzLzAzLzMyLzI5/LzM2MF9GXzMwMzMy/Mjk5NV9VY3FDT1lR/VzF1a05ma3BNVzBu/R0l2eGlpSGhGR2Uw/Yi5qcGc"
+                        }
+                        newUrl={element.url}
+                      />
+                    </div>
+                  </>
                 );
               })
             : null}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            type="button"
+            className="btn btn-dark"
+            disabled={this.state.page <= 1}
+            onClick={this.handlePrevClick}
+          >
+            &larr; prev
+          </button>
+          <button
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            next &rarr;
+          </button>
         </div>
       </div>
     );
